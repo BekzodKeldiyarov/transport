c7843d4109605385becb74cbb0fbc23fa945c5ec
Bekzod <bek.bek.bek.bek72@gmail.com>
2021-02-13 17:53:36 +0500

    - return application page - validation started

---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+$().ready(function(){
+    $('#return-application-form').validate({
+        rules: {
+            socialSecuryNumber: {
+                required: true,
+                minlength: 6
+            },
+            repeatSocialsecuritynumber: {
+                required: true,
+                minlength: 6,
+                equalTo: "#social-security-number"
+            }
+        },
+        messages: {
+            socialSecurityNumber: "Please enter the social security number",
+            repeatSocialsecuritynumber: "Repeat the social security number"
+        }
+    });
+
+    $('#applicant-form').validate({
+        rules: {
+            applicantFirstName: {
+                required: true,
+                minlength: 2
+            },
+            applicantMiddleName:{
+                required: true,
+                minlength: 2
+            },
+            applicantLastName:{
+                required: true,
+                minlength: 2,
+                number: false
+            },
+            // applicantSuffix: {
+            //     required: true,
+            //     SelectName: { valueNotEquals: "" }
+
+            // },
+            repeatSocialsecuritynumber: {
+                required: true,
+                minlength: 6,
+                equalTo: "#social-security-number"
+            },
+            applicantEmail:{
+                required: true,
+                email: true
+            },
+            // applicantTwicCard:{
+            //     required: true,
+            //     SelectName: { valueNotEquals: "" }
+            // },
+            // applicantPassport:{
+            //     required: true,
+            //     SelectName: { valueNotEquals: "" }
+            // }
+            driverFirstName:{
+                required: true,
+                minlength: 2
+            },
+            driverLastName:{
+                required: true,
+                minlength: 2
+            }
+        },
+        messages: {
+            socialSecurityNumber: "Please enter the social security number",
+            repeatSocialsecuritynumber: "Repeat the social security number",
+        }
+    });
+
+
+    $("#social-security-number").inputmask({
+        "mask": "999-99-9999"
+    });
+    $("#applicant-phone-number, #applicant-alt-number").inputmask({
+        "mask": "(999) 999-99-9999"
+    })
+
+})
+
